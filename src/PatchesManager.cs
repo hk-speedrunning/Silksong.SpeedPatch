c8b5ecc7561b3afe440d67bef853b7df2e0b4f2c
peekagrub <25369813+peekagrub@users.noreply.github.com>
2026-07-28 23:30:32 +0200

    Add "-SP" to patch number to indicate SpeedPatch

--- src/PatchesManager.cs
+++ src/PatchesManager.cs
@@ -59,6 +59,7 @@ internal class PatchesManager
 
         _patches = new List<Patch> {
             new OnGUIPatch(_targetModule, _sourceModule, _settings),
+            new VersionPatch(_targetMainModule),
         };
 
         if (_settings.DowndashTransitionFix)
