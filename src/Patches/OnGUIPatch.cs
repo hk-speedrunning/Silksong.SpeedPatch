759f4b2fbfefd23ba80c17ad30ac1079c6e728f5
peekagrub <25369813+peekagrub@users.noreply.github.com>
2026-07-29 13:18:01 +0200

    Add intro skip to settings and OnGUI

--- src/Patches/OnGUIPatch.cs
+++ src/Patches/OnGUIPatch.cs
@@ -30,6 +30,9 @@ internal class OnGUIPatch : CopyPatch
                     case "CourierRNGFix":
                         _warningText += "CourierRNGFix";
                         break;
+                    case "IntroSkip":
+                        _warningText += "IntroSkipPatch";
+                        break;
                 }
                 _warningText += '\n';
             }
