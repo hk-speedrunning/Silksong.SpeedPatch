ecef42f528f8bd2f70c7fac054ad10c495f6a56f
peekagrub <25369813+peekagrub@users.noreply.github.com>
2026-09-18 19:30:09 +0200

    Add TextMasher to warning text

--- src/Patches/OnGUIPatch.cs
+++ src/Patches/OnGUIPatch.cs
@@ -30,6 +30,9 @@ internal class OnGUIPatch : CopyPatch
                     case "CourierRNGFix":
                         _warningText += "CourierRNGFix";
                         break;
+                    case "TextMasher":
+                        _warningText += "TextMasherPatch";
+                        break;
                 }
                 _warningText += '\n';
             }
